d2a0bc4a6d99fb6b95b419d0cc1b35e705c2f729
hugoalh <32359235+hugoalh@users.noreply.github.com>
2021-11-03 12:10:54 +0800

    -

--- main.ps1
+++ main.ps1
@@ -7,24 +7,30 @@ param (
 )
 $arbitraryBoolean = [bool]::Parse($arbitrary)
 $dryRunBoolean = [bool]::Parse($dryRun)
-$payloadStringify = (ConvertFrom-Json -InputObject $payload | ConvertTo-Json -Depth 100 -Compress)
+$payloadStringify = (ConvertFrom-Json -InputObject $payload -Depth 100 | ConvertTo-Json -Depth 100 -Compress)
 $ghactionUserAgent = "TriggerIFTTTWebhookApplet.GitHubAction/4.0.0"
 if ($dryrun -eq $true) {
+	Write-Output -InputObject "Post network request to test service."
 	Write-Output -InputObject "Event Name: $eventName"
 	Write-Output -InputObject "Payload Content: $payloadStringify"
-	Write-Output -InputObject "Payload Length: $($payloadStringify.Length)"
-	$payloadFakeStringify = (ConvertFrom-Json -InputObject '{"body": "bar", "title": "foo", "userId": 1}' | ConvertTo-Json -Depth 100 -Compress)
+	$payloadFakeStringify = (ConvertFrom-Json -InputObject '{"body": "bar", "title": "foo", "userId": 1}' -Depth 100 | ConvertTo-Json -Depth 100 -Compress)
 	Write-Output -InputObject "Post network request to test service."
-	Invoke-WebRequest -UseBasicParsing -Uri "https://jsonplaceholder.typicode.com/posts" -UserAgent $ghactionUserAgent -Headers @{ "Content-Type" = "application/json"; "Content-Length" = $($payloadFakeStringify.Length) } -MaximumRedirection 5 -Method Post -Body $payloadFakeStringify -ContentType "application/json"
+	$response = Invoke-WebRequest -UseBasicParsing -Uri "https://jsonplaceholder.typicode.com/posts" -UserAgent $ghactionUserAgent -MaximumRedirection 5 -Method Post -Body $payloadFakeStringify -ContentType "application/json"
+	foreach ($element in $response) {
+		Write-Output -InputObject "$($element.Name): $($element.Value)"
+	}
 } else {
+	Write-Output -InputObject "Post network request to IFTTT."
 	Write-Output -InputObject "::debug::Event Name: $eventName"
 	Write-Output -InputObject "::debug::Payload Content: $payloadStringify"
-	Write-Output -InputObject "::debug::Payload Length: $($payloadStringify.Length)"
 	Write-Output -InputObject "Post network request to IFTTT."
 	$webRequestURL = "https://maker.ifttt.com/trigger/$eventname"
 	if ($arbitrary -eq $true) {
 		$webRequestURL += "/json"
 	}
 	$webRequestURL += "/with/key/$key"
-	Invoke-WebRequest -UseBasicParsing -Uri $webRequestURL -UserAgent $ghactionUserAgent -Headers @{ "Content-Type" = "application/json"; "Content-Length" = $($payloadStringify.Length) } -MaximumRedirection 5 -Method Post -Body $payloadStringify -ContentType "application/json"
+	$response = Invoke-WebRequest -UseBasicParsing -Uri $webRequestURL -UserAgent $ghactionUserAgent -MaximumRedirection 5 -Method Post -Body $payloadStringify -ContentType "application/json"
+	foreach ($element in $response) {
+		Write-Output -InputObject "::debug::$($element.Name): $($element.Value)"
+	}
 }
