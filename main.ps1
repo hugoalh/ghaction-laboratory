91e27d4e6f6af888e4106c11de986f5f7337e969
hugoalh <32359235+hugoalh@users.noreply.github.com>
2021-11-18 17:26:24 +0800

    -

--- main.ps1
+++ main.ps1
@@ -1,6 +1,6 @@
 $Mode = ($env:INPUT_MODE).ToLower()
 Write-Output -InputObject $Mode
-$CommitsRaw = $(git --no-pager log --format=%H)
+$CommitsRaw = $(/usr/bin/git --no-pager log --format=%H)
 $Commits = $CommitsRaw.Split("\r?\n")
 Write-Output -InputObject $($Commits.Length)
 Exit 0
