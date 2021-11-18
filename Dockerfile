71f46bbb9a83a0cc847204d8f94c245664ae3a86
hugoalh <32359235+hugoalh@users.noreply.github.com>
2021-11-18 16:41:50 +0800

    -

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
-FROM clamav/clamav:latest_base
-ENV CLAMAV_NO_CLAMD true
-ENV CLAMAV_NO_FRESHCLAMD true
-ENV CLAMAV_NO_MILTERD true
+# FROM clamav/clamav:latest_base
+# ENV CLAMAV_NO_CLAMD true
+# ENV CLAMAV_NO_FRESHCLAMD true
+# ENV CLAMAV_NO_MILTERD true
+FROM powershell:latest
 COPY main.ps1 /
-CMD ["/usr/bin/pwsh", "-c", "/main.ps1"]
+CMD ["/main.ps1"]
--- main.ps1
+++ main.ps1
@@ -1,12 +1,14 @@
 $mode = ($env:INPUT_MODE).ToLower()
-if (($mode -ne "fast") -and ($mode -ne "full")) {
-	Write-Output -InputObject "::error title=Error::Invalid mode!"
-}
-function Execute-ClamScan {
-	clamscan --official-db-only=yes --quiet --recursive .
-}
-Write-Output -InputObject "Scan current file system."
-Execute-ClamScan
-if ($mode -eq "full") {
-
-}
+Write-Output -InputObject $mode
+Exit 0
+# if (($mode -ne "fast") -and ($mode -ne "full")) {
+# 	Write-Output -InputObject "::error title=Error::Invalid mode!"
+# }
+# function Execute-ClamScan {
+# 	clamscan --official-db-only=yes --quiet --recursive .
+# }
+# Write-Output -InputObject "Scan current file system."
+# Execute-ClamScan
+# if ($mode -eq "full") {
+# 
+# }
