c97d01b34c64678da8a6c899b3e116183c0be18c
hugoalh <32359235+hugoalh@users.noreply.github.com>
2021-11-18 18:20:45 +0800

    -

--- main.ps1
+++ main.ps1
@@ -1,8 +1,9 @@
 $Mode = ($env:INPUT_MODE).ToLower()
 Write-Output -InputObject $Mode
 $CommitsRaw = $(git --no-pager log --format=%H)
-$Commits = $CommitsRaw.Split("\r?\n")
+$Commits = [regex]::split($CommitsRaw, "\r?\n")
 Write-Output -InputObject $($Commits.Length)
+Write-Output -InputObject $Commits
 Exit 0
 # if (($Mode -ne "fast") -and ($Mode -ne "full")) {
 # 	Write-Output -InputObject "::error title=Error::Invalid mode!"
