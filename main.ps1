ca6bb0b224d806ce203427147008cf7f86cd04e5
hugoalh <32359235+hugoalh@users.noreply.github.com>
2021-11-19 16:44:09 +0800

    -

--- main.ps1
+++ main.ps1
@@ -1,39 +1,44 @@
 $GitDepth = [bool]::Parse($env:INPUT_GITDEPTH)
-[uint64]$ElementsCount = 0
+$ElementsTotalCount = 0
 $SetError = $false
 function Execute-Scan {
 	param (
-		[Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true)][string]$Session,
+		[Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true)][string]$Message,
 		[Parameter()][switch]$SkipGitDatabase
 	)
-	$ElementsCount += $(Get-ChildItem -Force -Name -Path .\ -Recurse).Length
+	Write-Output -InputObject "::group::Scan $Message."
+	$Elements = $(Get-ChildItem -Force -Name -Path .\ -Recurse)
+	Write-Output -InputObject "::debug::Directory elements ($($Elements.Length)):"
+	foreach ($Element in $Elements) {
+		Write-Output -InputObject "::debug::- $Element"
+	}
+	$ElementsTotalCount += $Elements.Length
 	$Result
 	if ($SkipGitDatabase -eq $true) {
-		$ElementsCount -= $($(Get-ChildItem -Force -Name -Path .\.git -Recurse).Length - 1)
-		$Result = $(clamscan --exclude=./.git --official-db-only=yes --quiet --recursive .)
+		$ElementsTotalCount -= $($(Get-ChildItem -Force -Name -Path .\.git -Recurse).Length + 1)
+		$Result = $(clamscan --exclude=./.git --official-db-only=yes --recursive ./)
 	} else {
-		$Result = $(clamscan --official-db-only=yes --quiet --recursive .)
+		$Result = $(clamscan --official-db-only=yes --recursive ./)
 	}
-	if ($Result -notmatch "^\s*$") {
+	if ($Result -match "found") {
 		$SetError = $true
-		Write-Output -InputObject "::error::Found virus at $($Session): $Result"
+		Write-Output -InputObject "::error::Found virus!"
 	}
+	Write-Output -InputObject "::debug::$Result"
+	Write-Output -InputObject "::endgroup::"
 }
-Write-Output -InputObject "Scan current workspace."
-Execute-Scan -Session "current workspace"
+Execute-Scan -Message "current workspace"
 if ($GitDepth -eq $true) {
 	if ($(Test-Path -Path .\.git) -eq $true) {
-		$Commits = (git --no-pager log --format=%H) -split "\r?\n"
+		$Commits = $($(git --no-pager log --format=%H) -split "\r?\n")
 		if ($Commits -ne $null) {
-			if ($Commits.Length -gt 1) {
-				for ($CommitIndex = 0; $CommitIndex -lt $Commits.Length; $CommitIndex++) {
-					Write-Output -InputObject "Scan commit $($Commits[$CommitIndex]) ($($CommitIndex + 1)/$($Commits.Length))."
-					git checkout --quiet "$($Commits[$CommitIndex])"
-					Execute-Scan -Session "commit $($Commits[$CommitIndex])" -SkipGitDatabase
-				}
-			} else {
-				Write-Output -InputObject "::warning::Current Git repository has only $($Commits.Length) commits! If this is incorrect, please modify ``actions/checkout`` with input ``fetch-depth`` to ``0``."
-				Exit 0
+			if ($Commits.Length -le 1) {
+				Write-Output -InputObject "::warning::Current Git repository has only $($Commits.Length) commits! If this is incorrect, please define ``actions/checkout`` input ``fetch-depth`` to ``0`` and re-run. (IMPORTANT: Press the ``Re-run all jobs`` or ``Re-run this workflow`` button cannot apply the modified workflow!)"
+			}
+			for ($CommitIndex = 0; $CommitIndex -lt $Commits.Length; $CommitIndex++) {
+				Write-Output -InputObject "Checkout commit #$($CommitIndex + 1)/$($Commits.Length): $($Commits[$CommitIndex])."
+				git checkout "$($Commits[$CommitIndex])"
+				Execute-Scan -Message "commit $($Commits[$CommitIndex])" -SkipGitDatabase
 			}
 		} else {
 			$SetError = $true
@@ -43,7 +48,7 @@ if ($GitDepth -eq $true) {
 		Write-Output -InputObject "::warning::Current workspace is not a Git repository!"
 	}
 }
-Write-Output -InputObject "Scanned elements: $ElementsCount"
+Write-Output -InputObject "Scanned elements total: $ElementsTotalCount"
 if ($SetError -eq $true) {
 	Exit 1
 }
