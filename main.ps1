d7cc0d49a3b82942be16c849afa9e10c9c6fd783
hugoalh <32359235+hugoalh@users.noreply.github.com>
2021-11-19 12:46:27 +0800

    -

--- main.ps1
+++ main.ps1
@@ -1,17 +1,37 @@
 $GitDepth = [bool]::Parse($env:INPUT_GITDEPTH)
-Write-Output -InputObject "::debug::Git Depth: $GitDepth"
-$Commits = (git --no-pager log --format=%H) -split "\r?\n"
-Write-Output -InputObject $($Commits.Length)
-Write-Output -InputObject $Commits
-Exit 0
-# if (($Mode -ne "fast") -and ($Mode -ne "full")) {
-# 	Write-Output -InputObject "::error title=Error::Invalid mode!"
-# }
-# function Execute-ClamScan {
-# 	clamscan --official-db-only=yes --quiet --recursive .
-# }
-# Write-Output -InputObject "Scan current file system."
-# Execute-ClamScan
-# if ($Mode -eq "full") {
-# 
-# }
+$SetError = $false
+function Execute-Scan {
+	param (
+		[Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true)][string]$Session
+	)
+	$Result = $(clamscan --official-db-only=yes --quiet --recursive .)
+	if ($Result -notmatch "^\s*$") {
+		$SetError = $true
+		Write-Output -InputObject "::error::Found virus at $($Session): $Result"
+	}
+}
+Write-Output -InputObject "Scan current workspace."
+Execute-Scan -Session "current workspace"
+if ($GitDepth -eq $true) {
+	if ($(Test-Path -Path .\.git) -eq $false) {
+		Write-Output -InputObject "::warning::Current workspace is not a Git repository!"
+		Exit 0
+	}
+	$Commits = (git --no-pager log --format=%H) -split "\r?\n"
+	if ($Commits -eq $null) {
+		Write-Output -InputObject "::error::Current workspace is not a valid Git repository!"
+		Exit 1
+	}
+	if ($Commits.Length -le 1) {
+		Write-Output -InputObject "::warning::Current Git repository has only $($Commits.Length) commits! If this is incorrect, please modify ``actions/checkout`` with input ``fetch-depth`` to ``0``."
+		Exit 0
+	}
+	for ($CommitIndex = 0; $CommitIndex -lt $Commits.Length; $CommitIndex++) {
+		Write-Output -InputObject "Scan commit $($Commits[$CommitIndex]) ($CommitIndex/$($Commits.Length))."
+		git checkout "$($Commits[$CommitIndex])"
+		Execute-Scan -Session "commit $($Commits[$CommitIndex])"
+	}
+}
+if ($SetError -eq $true) {
+	Exit 1
+}
