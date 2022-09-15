82e56bb44a796f75738cca7e37b42745fbbae456
hugoalh <32359235+hugoalh@users.noreply.github.com>
2022-09-15 14:20:09 +0800

    -

--- main.ps1
+++ main.ps1
@@ -1,4 +1,4 @@
 Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath 'git.psm1') -Scope 'Local'
 Get-GitCommitsInformation |
-	Format-Table -AutoSize -Wrap |
+	Format-Table -Property '*' -AutoSize -Wrap |
 	Out-String
